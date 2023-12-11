a54b1eeee0367fff2442fcd78b85f6dc049f82f6
Luigi Matrone <86515515+Gigiotelli@users.noreply.github.com>
2023-12-11 14:27:47 +0100

    added InputField, numberButtonAction, btnCAction
    Autofocus sull'inputField;
    Clear input attraverso il tasto "C";
    Inserimento valori "0-9, j, ." attraverso i bottoni dell'interfaccia;

--- src/it/unisa/diem/ids/project/view/View.java
+++ src/it/unisa/diem/ids/project/view/View.java
@@ -7,6 +7,7 @@ package it.unisa.diem.ids.project.view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -22,6 +23,7 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  *
@@ -36,12 +38,22 @@ public class View implements Initializable {
     private Label label;
     private Button closeButton;
     private Button minimizeButton;
+    
+    @FXML
     private Button btnC;
+    
+    @FXML
+    private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnDot, btnIm;
+    private String buffer = new String();
+    
+    @FXML
     ChoiceBox choiceVar = new ChoiceBox<>(
                 FXCollections.observableArrayList(
                         'a','b','c'
                 ));
+    @FXML
     private TextField inputField;
+      
     
     @FXML
     private void diplayVarsAction(ActionEvent event) {
@@ -62,15 +74,57 @@ public class View implements Initializable {
     }
     
     @FXML
-    private void btnCAction (ActionEvent event) {
+    private void bottoneProvaAction(ActionEvent event) {
+        // TODO
+         
+    }
     
-       System.out.println("CLICCATO BTN C");
+    @FXML
+    private void btnCAction (ActionEvent event) {
+        inputField.clear();
+        inputField.requestFocus();
+    } 
+    
+    @FXML
+    private void numberButtonAction (ActionEvent event) {        
+        Object source = event.getSource();
+        buffer = inputField.getText();
+	if(source==btn0)
+            inputField.setText(buffer+"0");
+	if(source==btn1)
+            inputField.setText(buffer+"1");
+	if(source==btn2)
+            inputField.setText(buffer+"2");
+	if(source==btn3)
+            inputField.setText(buffer+"3");
+	if(source==btn4)
+            inputField.setText(buffer+"4");
+	if(source==btn5)
+            inputField.setText(buffer+"5");
+	if(source==btn6)
+            inputField.setText(buffer+"6");
+	if(source==btn7)
+            inputField.setText(buffer+"7");
+	if(source==btn8)
+            inputField.setText(buffer+"8");
+	if(source==btn9)
+            inputField.setText(buffer+"9");
+	if(source==btnDot)
+            inputField.setText(buffer+".");
+	if(source==btnIm)
+            inputField.setText(buffer+"j");
+        
     }
    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        
+        Platform.runLater(new Runnable() {
+        @Override
+        public void run() {
+            inputField.requestFocus();
+        }
+    });
     }    
     
     public void displayTabView() {
