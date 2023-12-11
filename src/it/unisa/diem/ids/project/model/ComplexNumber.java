be2fd0cc9048f42a1fd2eb640fd03a7027a48eff
Andrey Kiriyanov <kiriyanovandrey.eng@gmail.com>
2023-12-11 10:55:22 +0100

    funzionalità individuali in complex number implementate

--- src/it/unisa/diem/ids/project/model/ComplexNumber.java
+++ src/it/unisa/diem/ids/project/model/ComplexNumber.java
@@ -15,8 +15,15 @@ public class ComplexNumber implements ComplexNumberOperation{
     private double im;
     
     // COSTRUTTORE
-    public ComplexNumber(){
-        
+    public ComplexNumber(double re, double im){
+        this.re=re;
+        this.im=im;
+    }
+    
+    // COSTRUTTORE di un numero reale
+    public ComplexNumber(double re){
+        this.re=re;
+        this.im=0.0;
     }
     
     // TO STRING
@@ -42,35 +49,70 @@ public class ComplexNumber implements ComplexNumberOperation{
         this.im = im;
     }
     
-    // METODI DELL'INTERFACCIA
+    // METODI PER LE FUNZIONALITA ARITMETICHE INDIVIDUALI 
     @Override
     public ComplexNumber add(ComplexNumber c) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        double newRe = this.re + c.re;
+        double newIm = this.im + c.im;
+        
+        return new ComplexNumber(newRe, newIm);
     }
 
     @Override
     public ComplexNumber sub(ComplexNumber c) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        double newRe = this.re - c.re;
+        double newIm = this.im - c.im;
+        
+        return new ComplexNumber(newRe, newIm);
     }
 
     @Override
     public ComplexNumber multiply(ComplexNumber c) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        double newRe = this.re * c.re - this.im * c.im;     //moltiplicazione delle parti reali e immaginarie (j^2 è reale)
+        double newIm = this.re * c.im + this.im * c.re;     //termini con coefficente immaginario
+        
+        return new ComplexNumber(newRe, newIm);
     }
 
     @Override
-    public ComplexNumber div(ComplexNumber c) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public ComplexNumber div(ComplexNumber c) {            //   a+bi/c+di=(ac+bd)/(c^2+d^2)+i⋅(bc−ad)/(c^2+d^2) formula divisione di numeri complessi, entrambi 
+      
+        double den = c.re * c.re + c.im * c.im;   //denominatore
+
+        double newRe = (this.re * c.re + this.im * c.im) / den;
+        double newIm = (this.im * c.re - this.re * c.im) / den;
+
+        return new ComplexNumber(newRe, newIm);
     }
 
     @Override
     public ComplexNumber sqrt() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        int sign;
+        if(im >= 0)
+            sign = 1;
+        else
+            sign = -1;
+        
+        double r = Math.sqrt(this.re * this.re + this.im * this.im);            //applicazione della formula di De Moivre
+        double newRe = Math.sqrt((r + this.re) / 2);
+        double newIm = Math.sqrt((r - this.re) / 2) * sign;
+        
+        
+        return new ComplexNumber(newRe, newIm);
+        
     }
 
     @Override
     public ComplexNumber reverseSign() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        double newRe = this.re * -1;
+        double newIm = this.im * -1;
+        
+        return new ComplexNumber(newRe, newIm);
     }
     
 }
